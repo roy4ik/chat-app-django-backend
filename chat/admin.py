cb98dd607f4f9a59e607bd7890db4706b9ed4627
roy4ik <royaltstaedter@gmail.com>
2021-08-26 00:21:14 +0300

    added multi recipient support via conversation participants and added read_statuses to messages. Added date_created and date_updated to Message model

--- chat/admin.py
+++ chat/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from accounts.models import Profile
-from chat.models import Conversation, Message
+from chat.models import Conversation, Message, ReadStatus
 
 admin.site.register(Profile)
 admin.site.register(Conversation)
 admin.site.register(Message)
+admin.site.register(ReadStatus)
--- chat/models.py
+++ chat/models.py
@@ -1,22 +1,58 @@
-from django.db import models
+import datetime
+
+from django.db import models, transaction
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Conversation(models.Model):
     is_active = models.BooleanField(default=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                   blank=False, null=False)
     date_created = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
+    participants = models.ManyToManyField(User, related_name='participants')
 
 
 class Message(models.Model):
     conversation = models.OneToOneField(Conversation, on_delete=models.CASCADE, blank=False, null=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
     is_active = models.BooleanField(default=True)
     subject = models.CharField(max_length=128, blank=False, null=False)
     content = models.CharField(max_length=1024)
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    read_statuses = models.ManyToManyField('ReadStatus')
 
     def __str__(self):
-        return f'{self.id}: {self.created_by} -> {self.recipient}'
+        return f'{self.id}: {self.created_by}'
+
+    class Meta:
+        ordering = ['date_created']
+
+
+@receiver(post_save, sender=Message)
+def set_read_statuses(sender, created, instance, **kwargs):
+    """sets the read statuses on the message for all participants of a conversation
+        at the  time of message creation
+     """
+    if created:
+        with transaction.atomic():
+            participants = instance.conversation.participants.all()
+            instance.read_statuses.add(*[ReadStatus.objects.create(
+                recipient=recipient) for recipient in participants])
+
+
+class ReadStatus(models.Model):
+    is_active = models.BooleanField(default=True)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False, related_name='recipient')
+    date_read = models.DateField(null=True)
+
+    def is_read(self):
+        return bool(self.date_read)
+
+    def set_read(self):
+        self.date_read = datetime.datetime.utcnow()
+        return self.is_read()
