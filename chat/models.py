fb4acd82711768c04c461c059ec0a96724ecbe55
roy4ik <royaltstaedter@gmail.com>
2021-08-27 14:04:22 +0300

    added name to Conversation changed Message read_statuses to recipients changed Read Status to Recipient (refactor) changed set_read_statuses to set_initial_recipients added Meta: ordering to conversations

--- chat/models.py
+++ chat/models.py
@@ -9,22 +9,26 @@ from django.dispatch import receiver
 # Create your models here.
 class Conversation(models.Model):
     is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=128, blank=False, null=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE,
                                    blank=False, null=False)
     date_created = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
     participants = models.ManyToManyField(User, related_name='participants')
 
+    class Meta:
+        ordering = ['date_created']
+
 
 class Message(models.Model):
-    conversation = models.OneToOneField(Conversation, on_delete=models.CASCADE, blank=False, null=False)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, blank=False, null=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     date_created = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
     is_active = models.BooleanField(default=True)
     subject = models.CharField(max_length=128, blank=False, null=False)
     content = models.CharField(max_length=1024)
-    read_statuses = models.ManyToManyField('ReadStatus')
+    recipients = models.ManyToManyField('Recipient', related_name='read_statuses')
 
     def __str__(self):
         return f'{self.id}: {self.created_by}'
@@ -34,25 +38,36 @@ class Message(models.Model):
 
 
 @receiver(post_save, sender=Message)
-def set_read_statuses(sender, created, instance, **kwargs):
+def set_initial_recipients(sender, created, instance, **kwargs):
     """sets the read statuses on the message for all participants of a conversation
         at the  time of message creation
      """
     if created:
         with transaction.atomic():
-            participants = instance.conversation.participants.all()
-            instance.read_statuses.add(*[ReadStatus.objects.create(
+            participants = instance.conversation.participants.all().exclude(id=instance.created_by.id)
+            instance.recipients.add(*[Recipient.objects.create(
+                message=instance,
                 recipient=recipient) for recipient in participants])
 
 
-class ReadStatus(models.Model):
+class Recipient(models.Model):
+    message = models.ForeignKey(Message,
+                                on_delete=models.CASCADE,
+                                blank=False,
+                                null=False)
     is_active = models.BooleanField(default=True)
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False, related_name='recipient')
+    recipient = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  blank=False,
+                                  null=False,
+                                  related_name='recipient')
     date_read = models.DateField(null=True)
 
     def is_read(self):
         return bool(self.date_read)
 
     def set_read(self):
-        self.date_read = datetime.datetime.utcnow()
+        with transaction.atomic():
+            self.date_read = datetime.datetime.utcnow()
+            self.save()
         return self.is_read()
