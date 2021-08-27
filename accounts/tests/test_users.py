e28f514bdc35ad558a3517d5ca986c10ad60ad52
roy4ik <royaltstaedter@gmail.com>
2021-08-27 14:11:48 +0300

    moved tests and fixed function naming

--- accounts/tests/test_users.py
+++ accounts/tests/test_users.py
@@ -1,14 +1,15 @@
 from django.test import TransactionTestCase
 from accounts.models import User
+from django.test import TestCase
 # Create your tests here.
 
 
-class UserProfileCreationTestCase(TransactionTestCase):
+class UserProfileCreationTestCase(TestCase):
 
     def setUp(self):
         self.sender = User(
             username='jacob', email='jacob@gmail.com', password='top_secret')
 
-    def profile_creation_test(self):
+    def test_profile_creation(self):
         self.setUp()
         assert self.sender
