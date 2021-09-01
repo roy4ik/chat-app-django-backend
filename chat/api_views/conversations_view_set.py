dd2336bb01caa24dfd5f5f7a5fa89e3dbf9f7ede
roy4ik <royaltstaedter@gmail.com>
2021-09-01 23:36:45 +0300

    moved queryset to get queryset where possible. Improved Docstrings

--- chat/api_views/conversations_view_set.py
+++ chat/api_views/conversations_view_set.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from chat.models import Conversation
 from chat.serializers.conversations_serializers import ConversationSerializer
 from rest_framework.decorators import action
-from typing import Union, List
 from django.db.models import QuerySet
 
 
@@ -15,25 +14,28 @@ class ConversationViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = [authentication.TokenAuthentication]
 
-    def list(self, request, pk=None, **kwargs) -> Response:
-        """returns all the user's conversation including where he only participates"""
-        queryset = Conversation.objects.filter(Q(created_by=request.user)
-                                               | Q(participants__exact=request.user)).distinct()
+    def list(self, request, **kwargs) -> Response:
+        """Returns all the user's conversation including where he only participates."""
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated], url_path='owned')
+    def list_owned_conversations(self, request, **kwargs) -> Response:
+        """Returns all the user's conversations that he created/owns."""
+        queryset = Conversation.objects.filter(created_by=self.request.user).distinct()
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated], url_path='/owned')
-    def list_owned_conversations(self, request, pk=None, **kwargs) -> Response:
-        """returns all the user's conversations that he created/owns"""
-        queryset = Conversation.objects.filter(created_by=request.user)
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False, methods=['get'])
-    def get_queryset(self) -> Union[QuerySet, List[Conversation]]:
-        """
-        get all conversations that the user participates in.
-        note: this is only for get methods as this includes other user's conversations
-        """
-        return Conversation.objects.filter(Q(created_by=self.request.user)
-                                           | Q(participants__exact=self.request.user)).distinct()
+    def get_queryset(self) -> QuerySet:
+        if self.request.method == 'GET':
+            # get all conversations that the user participates in.
+            # note: this is only for get methods as this includes other user's conversations
+            if self.action == 'retrieve':
+                Conversation.objects.filter(Q(created_by=self.request.user)
+                                            | Q(participants__exact=self.request.user),
+                                            pk=self.kwargs.get('pk')).distinct()
+            return Conversation.objects.filter(Q(created_by=self.request.user)
+                                               | Q(participants__exact=self.request.user)).distinct()
+        else:
+            pk = self.kwargs.get('pk')
+            return Conversation.objects.filter(created_by=self.request.user, pk=pk).distinct()
