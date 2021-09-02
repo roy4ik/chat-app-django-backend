2b983dc5eaf332814ed1e4499263b1ceb3ed097d
roy4ik <royaltstaedter@gmail.com>
2021-09-02 04:26:06 +0300

    setting static files

--- django_rest_chat/prod_settings.py
+++ django_rest_chat/prod_settings.py
@@ -9,16 +9,26 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from pathlib import Path
 import os
-# import confuse
 import django_heroku
 
 # !!!!! SET DJANGO_SETTINGS_MODULE=django_rest_chat.prod_settings in your environment variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # set DJANGO_SETTINGS_MODULE=django_rest_chat.dev_settings
 # Configuration file
 # CONFIG = confuse.Configuration('rest-chat')
@@ -34,7 +44,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
